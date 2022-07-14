5a429418db5eb35064be9f52d7a124f1b4acb90b
lucastine <lucastine@poli.ufrj.br>
2022-07-14 12:35:25 -0300

    starting

---
+++ src/components/PsHeader.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <div>
+            <h1> PhotoSpice Web </h1>
+            <p1> Circuit analysis on the fly </p1>
+        </div>
+    </header>
+</template>
